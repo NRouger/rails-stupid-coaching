55f8e86fcdfb4660ad1f15a64069b75782f8c9c8
nicolas rouger <nicorouger@outlook.com>
2022-11-14 16:22:41 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,12 +11,5 @@ class QuestionsController < ApplicationController
     else
       @answer = "I don't care, get dressed and go to work!"
     end
-
-    # @answers = ['Great!', 'Silly=begin  question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
-
-    # if :ask == "I am going to work" then answers:[0]
-    # elsif :ask.end_with ? then answers:[1]
-    # elseif :ask == return answers:[2]
-    # end =end
   end
 end
